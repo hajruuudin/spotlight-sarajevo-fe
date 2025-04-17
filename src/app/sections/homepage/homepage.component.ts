df3a17c5c15391e58d6981d131b11c69524a8844
hajruuudin <hajruuudin@gmail.com>
2025-04-17 22:20:37 +0200

    Homepage: Headlines for spots

--- src/app/sections/homepage/homepage.component.ts
+++ src/app/sections/homepage/homepage.component.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeadingComponent } from "../../components/heading/heading.component";
 import { HttpClient } from '@angular/common/http';
 import { TagService } from '../../services/tag.service';
+import { SpotHeadlineComponent } from "../../components/spot-headline/spot-headline.component";
+import { SpotShorthand } from '../../models/spot-model';
+import { SpotService } from '../../services/spot.service';
 
 @Component({
   selector: 'app-homepage',
-  imports: [HeadingComponent],
+  imports: [HeadingComponent, SpotHeadlineComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
-export class HomepageComponent {
-  constructor(private tagService : TagService) {}
+export class HomepageComponent implements OnInit{
+  headlineSpot : SpotShorthand | null = null;
+  navigateToSpotOverview: (spotId: number) => void = () => {console.log("Clicked")};
 
-  fetchData(){
-    this.tagService.fetchData()
+  constructor(
+    private tagService : TagService,
+    private spotService : SpotService
+  ) {}
+
+  ngOnInit(): void {
+    this.spotService.getHeadlineSpot().subscribe({
+      next: (response : any) => {
+        this.headlineSpot = response
+      },
+      error: (error : Error) => {
+        console.error()
+      }
+    })
   }
+
+
 }
