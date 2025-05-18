0591520c30239acc691398c86fbb6a63aefd3dda
Hajrudin Imamovic <hajruuudin@gmail.com>
2025-05-18 19:40:23 +0200

    Event Overview: Full Page

--- src/app/models/event-model.ts
+++ src/app/models/event-model.ts
@@ -8,3 +8,30 @@ export interface EventShorthand{
     tagNames: string[],
     imageUrl: string;
 }
+
+export class EventModel {
+  constructor(
+    public id: number,
+    public slug: string,
+    public officialName: string,
+    public smallDescription: string,
+    public fullDescription: string,
+    public categoryName: string,
+    public tagNames: string[],
+    public startDate: string,
+    public endDate: string,
+    public entryPrice: number,
+    public ageLimit: number,
+    public reservation: boolean,
+    public cancelRefund: string,
+    public openStatus: string,
+    public eventLanguage: string,
+    public address: string,
+    public locationDescription: string,
+    public imageUrl: string,
+    public created: string,
+    public createdBy: string,
+    public modified: string,
+    public modifiedBy: string
+  ) {}
+}
