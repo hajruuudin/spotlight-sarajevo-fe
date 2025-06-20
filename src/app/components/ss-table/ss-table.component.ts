919a9a60138810373f9d68dd143b4cf0fac89496
Hajrudin Imamovic <hajruuudin@gmail.com>
2025-06-20 21:52:51 +0200

    Data Review: Initial table & form desing

--- src/app/components/ss-table/ss-table.component.ts
+++ src/app/components/ss-table/ss-table.component.ts
@@ -1,26 +1,146 @@
-import { Component, Input } from '@angular/core';
-import { SpotShorthand } from '../../models/spot-model';
-import { EventShorthand } from '../../models/event-model';
+import { Component, Input, OnInit } from '@angular/core';
+import { SpotModel, SpotShorthand } from '../../models/spot-model';
+import { EventModel, EventShorthand } from '../../models/event-model';
 import { DatePipe, NgFor, NgIf } from '@angular/common';
+import { SpotService } from '../../services/spot.service';
+import { EventService } from '../../services/event.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+import { TagService } from '../../services/tag.service';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CategoryService } from '../../services/category.service';
 
 @Component({
   selector: 'app-ss-table',
-  imports: [NgFor, NgIf, DatePipe],
+  imports: [NgFor, NgIf, DatePipe, NgMultiSelectDropDownModule, ReactiveFormsModule, FormsModule],
   templateUrl: './ss-table.component.html',
   styleUrl: './ss-table.component.css',
   host: {
     class: 'w-full h-auto'
   }
 })
-export class SsTableComponent {
+export class SsTableComponent implements OnInit{
   @Input() tableType : Boolean = false;
   @Input() tableData: (SpotShorthand | EventShorthand)[] = [];
 
-  isSpot(row: SpotShorthand | EventShorthand): row is SpotShorthand {
-    return 'rating' in row;
+  protected isItemInfoShowed: Boolean = false;
+  protected selectedSlug: string = ''
+  protected fullItemInfo: (SpotModel | EventModel) | null = null;
+
+  protected tagDropdownSettings = {
+    singleSelection: false,
+    idField: 'tagId',
+    textField: 'tagName',
+    selectAllText: 'Select All',
+    unSelectAllText: 'UnSelect All',
+    itemsShowLimit: 3,
+    allowSearchFilter: true
+  }
+  protected tagDropdownSelectedItems : string[]= []
+  protected categorySelectItems : any = {}
+  protected tagDropdownAllItems : {tagId: number, tagName: string}[] = []
+  protected detailsSectionForm : FormGroup
+
+  constructor(
+    private spotService: SpotService,
+    private eventService: EventService,
+    private tagService: TagService,
+    private categoryService: CategoryService,
+    private fb: FormBuilder
+  ){ 
+    this.detailsSectionForm = this.fb.group({
+      "slug" : [''],
+      "officialName" : [''],
+      "smallDescription" : [''],
+      "fullDescription" : [''],
+      "categoryId" : [''],
+      "tagNames" : [[]],
+      "mondayTime" : [''],
+      "tuesdayTime" : [''],
+      "wednesdayTime" : [''],
+      "thursdayTime" : [''],
+      "fridayTime" : [''],
+      "saturdayTime" : [''],
+      "sundayTime" : ['']
+    })
   }
 
-  isEvent(row: SpotShorthand | EventShorthand): row is EventShorthand {
-    return 'startDateFormatted' in row;
+  ngOnInit(): void {
+    this.tagService.fetchData().subscribe({
+      next: (response : any) => {
+        let tagArray = response['content']
+        tagArray.forEach((tag : any) => {
+          this.tagDropdownAllItems.push({
+            tagId: tag.id,
+            tagName: tag.tagName
+          })
+        });
+      }
+    })
+
+    this.categoryService.fetchCategories().subscribe({
+      next: (response : any) => {
+        this.categorySelectItems = response['content']
+      }
+    })
+  }
+
+  isSpot(
+  row: SpotShorthand | SpotModel | EventShorthand | EventModel | null | undefined
+  ): row is SpotShorthand | SpotModel {
+    return !!row && 'rating' in row;
+  }
+
+  isEvent(
+    row: SpotShorthand | SpotModel | EventShorthand | EventModel | null | undefined
+  ): row is EventShorthand | EventModel {
+    return !!row && 'startDateFormatted' in row;
+  }
+
+
+  loadItemInfoSection(itemSlug: string, itemType: string){
+    this.selectedSlug = itemSlug;
+    this.isItemInfoShowed = true
+    this.getFullItemInfo(itemSlug, itemType);
+  }
+
+  closeItemInfoSection(){
+    this.isItemInfoShowed = false;
+    this.fullItemInfo = null;
+  }
+
+  getFullItemInfo(itemSlug: string, itemType: string){
+    if(itemType == "SPOT"){
+      this.spotService.getSpotOverviewBySlug(itemSlug).subscribe({
+        next: (response : any) => {
+          this.fullItemInfo = response as SpotModel
+          this.tagDropdownSelectedItems = this.fullItemInfo.tagNames
+        },
+        error: (error : HttpErrorResponse) => {
+          console.error(error)
+        }
+      })
+    } else if (itemType == "EVENT"){
+      this.eventService.getEventOverviewBySlug(itemSlug).subscribe({
+        next: (response : any) => {
+          this.fullItemInfo = response as EventModel
+        },
+        error: (error : HttpErrorResponse) => {
+          console.error(error)
+        }
+      })
+    }
+  }
+
+  onItemSelect(item: any){
+
+  }
+
+  onSelectAll(items: any){
+
+  }
+
+  labelWorkDayAsClosed(wordkDay: string){
+
   }
 }
--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarAdminComponent } from "../../components/navbar-admin/navbar-admin.component";
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-admin',
-  imports: [NavbarAdminComponent, RouterOutlet],
+  imports: [NavbarAdminComponent, RouterOutlet, FooterComponent],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
