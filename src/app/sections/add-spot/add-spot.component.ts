5f88cbe415d6c80a5a1d4a4c6772e1c30e5e754b
Hajrudin Imamovic <hajruuudin@gmail.com>
2025-06-24 21:40:13 +0200

    Admin Add Spot/Event: Spot Add Function

--- src/app/sections/add-spot/add-spot.component.ts
+++ src/app/sections/add-spot/add-spot.component.ts
@@ -6,10 +6,17 @@ import { NgFor, UpperCasePipe } from '@angular/common';
 import { TagService } from '../../services/tag.service';
 import { SpotService } from '../../services/spot.service';
 import { CategoryService } from '../../services/category.service';
+import { NgxSpinnerComponent, NgxSpinnerService } from 'ngx-spinner';
+import { HotToastService } from '@ngxpert/hot-toast';
+import { ButtonPrimaryComponent } from "../../components/button-primary/button-primary.component";
+import { MediaService } from '../../services/media.service';
+import { SpotCreateModel, SpotModel } from '../../models/spot-model';
+import { MediaCreate } from '../../models/media-model';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-spot',
-  imports: [NgFor, HeadingComponent, ReactiveFormsModule, NgMultiSelectDropDownModule, UpperCasePipe],
+  imports: [NgFor, HeadingComponent, ReactiveFormsModule, NgMultiSelectDropDownModule, UpperCasePipe, NgxSpinnerComponent, ButtonPrimaryComponent],
   templateUrl: './add-spot.component.html',
   styleUrl: './add-spot.component.css'
 })
@@ -30,21 +37,24 @@ export class AddSpotComponent implements OnInit {
   protected tagDropdownAllItems: { tagId: number, tagName: string }[] = []
   protected tagDropdownSelectedItems: { tagId: number; tagName: string }[] = [];
   protected categorySelectItems: any = {}
+  protected selectedFile: File | null = null;
 
   constructor(
     private tagService: TagService,
     private categoryService: CategoryService,
     private spotService: SpotService,
-    private fb: FormBuilder
+    private mediaService: MediaService,
+    private fb: FormBuilder,
+    private toastr: HotToastService,
+    private spinner: NgxSpinnerService
   ) {
     this.addForm = this.fb.group({
-      id: ['', Validators.required],
       slug: ['', Validators.required],
       officialName: ['', Validators.required],
       smallDescription: ['', Validators.required],
       fullDescription: ['', Validators.required],
       categoryName: ['', Validators.required],
-      tags: [[], Validators.required],
+      tags: [[]],
 
       // Work hours — optional
       mondayStartTime: [''],
@@ -98,15 +108,99 @@ export class AddSpotComponent implements OnInit {
   }
 
   onItemSelect(item: any) {
-
+    if (!this.tagDropdownSelectedItems.some(i => i.tagName === item.tagName)) {
+      this.tagDropdownSelectedItems.push(item);
+    }
   }
 
   onItemDeSelect(item: any) {
-
+    this.tagDropdownSelectedItems = this.tagDropdownSelectedItems.filter(i => i.tagName !== item.tagName);
   }
 
-  onItemSave(formData: any) {
+  async onItemSave() {
+    if (!this.addForm.valid) {
+      this.toastr.warning('All fields except event details need to be filled!')
+    } else {
+      this.spinner.show()
 
+      let imageUrl: string | null = null;
+
+      if (this.selectedFile) {
+        imageUrl = await this.mediaService.uploadImage(this.selectedFile);
+        console.log('Uploaded image URL:', imageUrl);
+      }
+
+      const formValues = this.addForm.value;
+
+      const newSpot = new SpotCreateModel(
+        formValues.officialName,
+        formValues.slug,
+        formValues.categoryName,
+        formValues.address,
+        Number(formValues.lat),
+        Number(formValues.lon),
+        formValues.fullDescription,
+        formValues.smallDescription,
+
+        formValues.mondayStartTime,
+        formValues.mondayEndTime,
+        formValues.tuesdayStartTime,
+        formValues.tuesdayEndTime,
+        formValues.wednesdayStartTime,
+        formValues.wednesdayEndTime,
+        formValues.thursdayStartTime,
+        formValues.thursdayEndTime,
+        formValues.fridayStartTime,
+        formValues.fridayEndTime,
+        formValues.saturdayStartTime,
+        formValues.saturdayEndTime,
+        formValues.sundayStartTime,
+        formValues.sundayEndTime,
+
+        Number(formValues.affordability),
+        Number(formValues.atmosphere),
+        Number(formValues.overallQuality),
+        Number(formValues.cleanliness),
+        Number(formValues.staffKindness),
+        Number(formValues.accessibility),
+
+        formValues.tags
+      );
+
+
+      this.spotService.addSpot(newSpot).subscribe({
+        next: (response: SpotModel) => {
+          this.spinner.hide()
+          this.toastr.success(`${response.officialName} added!`)
+
+          if (imageUrl != null) {
+            const mediaCreate = new MediaCreate(
+              "SPOT",
+              response.id,
+              imageUrl,
+              true
+            )
+
+            this.mediaService.addMedia(mediaCreate).subscribe({
+              next: (response: any) => { },
+              error: (response: HttpErrorResponse) => { }
+            })
+          }
+        },
+        error: (error: HttpErrorResponse) => {
+          this.spinner.hide()
+          this.toastr.error(error.message)
+        }
+      })
+    }
+  }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+      console.log('Selected file is:', this.selectedFile);
+    }
   }
 
   labelWorkDayAsClosed(workDay: string) {
