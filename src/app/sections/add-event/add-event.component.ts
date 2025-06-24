8c16709adbe75a2ccf2c6937dbd1420ff896421b
Hajrudin Imamovic <hajruuudin@gmail.com>
2025-06-24 08:46:34 +0200

    Add Spot/Event: Form Intiial Design (Tags needs fixing)

--- src/app/sections/add-event/add-event.component.ts
+++ src/app/sections/add-event/add-event.component.ts
@@ -1,12 +1,100 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeadingComponent } from "../../components/heading/heading.component";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+import { TagService } from '../../services/tag.service';
+import { CategoryService } from '../../services/category.service';
+import { HotToastService } from '@ngxpert/hot-toast';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ButtonPrimaryComponent } from "../../components/button-primary/button-primary.component";
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-add-event',
-  imports: [HeadingComponent],
+  imports: [NgFor, HeadingComponent, ReactiveFormsModule, NgMultiSelectDropDownModule, ButtonPrimaryComponent],
   templateUrl: './add-event.component.html',
   styleUrl: './add-event.component.css'
 })
-export class AddEventComponent {
+export class AddEventComponent implements OnInit{
+  protected addForm: FormGroup
+  protected tagDropdownSettings = {
+    singleSelection: false,
+    idField: 'tagId',
+    textField: 'tagName',
+    selectAllText: 'Select All',
+    unSelectAllText: 'UnSelect All',
+    itemsShowLimit: 5,
+    limitSelection: 5
+  }
+  protected tagDropdownAllItems: { tagId: number, tagName: string }[] = []
+  protected tagDropdownSelectedItems: { tagId: number; tagName: string }[] = [];
+  protected categorySelectItems: any = {}
 
+
+  constructor(
+    private tagService: TagService,
+    private categoryService: CategoryService,
+    private fb: FormBuilder,
+    private toastr: HotToastService,
+    private spinner: NgxSpinnerService
+  ){
+    this.addForm = this.fb.group({
+      slug: ['', Validators.required],
+      officialName: ['', Validators.required],
+      smallDescription: ['', Validators.required],
+      fullDescription: ['', Validators.required],
+      categoryName: ['', Validators.required],
+      tags: [[], Validators.required],
+
+      // Date and time
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required],
+      locationDescription: ['', Validators.required],
+      address: ['', Validators.required],
+
+      // Event specific data
+      entryPrice: [''],
+      ageLimit: [''],
+      reservation: [''],
+      cancelRefund: [''],
+      openStatus: [''],
+      eventLanguage: [''],
+    })
+  }
+
+  ngOnInit(): void {
+    this.tagService.fetchData().subscribe({
+      next: (response: any) => {
+        let tagArray = response['content']
+        tagArray.forEach((tag: any) => {
+          this.tagDropdownAllItems.push({
+            tagId: tag.id,
+            tagName: tag.tagName
+          })
+        });
+      }
+    })
+
+    this.categoryService.fetchCategories().subscribe({
+      next: (response: any) => {
+        this.categorySelectItems = response['content']
+      }
+    })
+  }
+
+  onItemSelect(item: any) {
+
+  }
+
+  onItemDeSelect(item: any) {
+
+  }
+
+  onItemSave() {
+    if(!this.addForm.valid){
+      this.toastr.warning('All fields except event details need to be filled!')
+    } else {
+      console.log(this.addForm)
+    }
+  }
 }
--- src/app/sections/add-spot/add-spot.component.ts
+++ src/app/sections/add-spot/add-spot.component.ts
@@ -1,12 +1,122 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeadingComponent } from "../../components/heading/heading.component";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+import { NgFor, UpperCasePipe } from '@angular/common';
+import { TagService } from '../../services/tag.service';
+import { SpotService } from '../../services/spot.service';
+import { CategoryService } from '../../services/category.service';
 
 @Component({
   selector: 'app-add-spot',
-  imports: [HeadingComponent],
+  imports: [NgFor, HeadingComponent, ReactiveFormsModule, NgMultiSelectDropDownModule, UpperCasePipe],
   templateUrl: './add-spot.component.html',
   styleUrl: './add-spot.component.css'
 })
-export class AddSpotComponent {
+export class AddSpotComponent implements OnInit {
+  protected addForm: FormGroup
+  protected days: string[] = [
+    'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'
+  ];
+  protected tagDropdownSettings = {
+    singleSelection: false,
+    idField: 'tagId',
+    textField: 'tagName',
+    selectAllText: 'Select All',
+    unSelectAllText: 'UnSelect All',
+    itemsShowLimit: 5,
+    limitSelection: 5
+  }
+  protected tagDropdownAllItems: { tagId: number, tagName: string }[] = []
+  protected tagDropdownSelectedItems: { tagId: number; tagName: string }[] = [];
+  protected categorySelectItems: any = {}
+
+  constructor(
+    private tagService: TagService,
+    private categoryService: CategoryService,
+    private spotService: SpotService,
+    private fb: FormBuilder
+  ) {
+    this.addForm = this.fb.group({
+      id: ['', Validators.required],
+      slug: ['', Validators.required],
+      officialName: ['', Validators.required],
+      smallDescription: ['', Validators.required],
+      fullDescription: ['', Validators.required],
+      categoryName: ['', Validators.required],
+      tags: [[], Validators.required],
+
+      // Work hours — optional
+      mondayStartTime: [''],
+      mondayEndTime: [''],
+      tuesdayStartTime: [''],
+      tuesdayEndTime: [''],
+      wednesdayStartTime: [''],
+      wednesdayEndTime: [''],
+      thursdayStartTime: [''],
+      thursdayEndTime: [''],
+      fridayStartTime: [''],
+      fridayEndTime: [''],
+      saturdayStartTime: [''],
+      saturdayEndTime: [''],
+      sundayStartTime: [''],
+      sundayEndTime: [''],
+
+      // Ratings — required
+      overallQuality: ['', Validators.required],
+      atmosphere: ['', Validators.required],
+      staffKindness: ['', Validators.required],
+      cleanliness: ['', Validators.required],
+      affordability: ['', Validators.required],
+      accessibility: ['', Validators.required],
+
+      // Location — required
+      address: ['', Validators.required],
+      lat: ['', Validators.required],
+      lon: ['', Validators.required]
+    })
+  }
+
+  ngOnInit(): void {
+    this.tagService.fetchData().subscribe({
+      next: (response: any) => {
+        let tagArray = response['content']
+        tagArray.forEach((tag: any) => {
+          this.tagDropdownAllItems.push({
+            tagId: tag.id,
+            tagName: tag.tagName
+          })
+        });
+      }
+    })
+
+    this.categoryService.fetchCategories().subscribe({
+      next: (response: any) => {
+        this.categorySelectItems = response['content']
+      }
+    })
+  }
+
+  onItemSelect(item: any) {
+
+  }
+
+  onItemDeSelect(item: any) {
+
+  }
+
+  onItemSave(formData: any) {
+
+  }
+
+  labelWorkDayAsClosed(workDay: string) {
+    const startControlName = `${workDay}StartTime`;
+    const endControlName = `${workDay}EndTime`;
+
+    this.addForm.patchValue({
+      [startControlName]: '',
+      [endControlName]: ''
+    });
+  }
 
 }
