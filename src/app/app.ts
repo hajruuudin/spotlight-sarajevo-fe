25e4092055f55d2609bd88f169a810fbfbae343e
Hajrudin Imamovic <hajruuudin@gmail.com>
2025-10-16 23:20:43 +0200

    003: Themes, Fonts, Variables & Dark Mode

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { environment } from '../environments/environment';
+import { HomepageIcon } from "./resources/icons/homepage-icon/homepage-icon";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HomepageIcon],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
