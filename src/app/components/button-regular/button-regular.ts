96e466987375af22fe05ccf906a2e747b60c6765
Hajrudin Imamovic <hajruuudin@gmail.com>
2025-10-28 22:59:37 +0100

    013: Small Documentation

--- src/app/components/button-regular/button-regular.ts
+++ src/app/components/button-regular/button-regular.ts
@@ -2,6 +2,19 @@ import { NgClass } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { GoogleIcon } from "../../resources/icons/google-icon/google-icon";
 
+/**
+ * ButtonRegular is a reusable button component.
+ * It supports optional icons, custom content, and additional CSS classes.
+ * Emits a `pressed` event when clicked.
+ *
+ * @example
+ * <app-button-regular
+ *   [buttonContent]="'Login with Google'"
+ *   [buttonIcon]="'GOOGLE'"
+ *   [classAddons]="'bg-blue-500 text-white'"
+ *   (pressed)="handleLogin()">
+ * </app-button-regular>
+ */
 @Component({
   selector: 'app-button-regular',
   imports: [NgClass, GoogleIcon],
